6301d8b46ffc03bcb178965d994ab9b184ad15dc
lboynton <notlee@gmail.com>
2008-08-18 12:49:32 +0000

    Changed security settings to high so that cookies expire sooner and the session id is regenerated, now that the applet is not in use.
    git-svn-id: svn+ssh://192.168.0.8/svn/backup/trunk@275 b4e1a2f0-0047-11dd-a6bb-41aab4003810

--- app/config/core.php
+++ app/config/core.php
@@ -133,7 +133,7 @@
  * When set to false, HTTP_USER_AGENT will not be checked
  * in the session
  */
-	Configure::write('Session.checkAgent', false);
+	Configure::write('Session.checkAgent', true);
 /**
  * The level of CakePHP security. The session timeout time defined
  * in 'Session.timeout' is multiplied according to the settings here.
@@ -146,7 +146,7 @@
  * CakePHP session IDs are also regenerated between requests if
  * 'Security.level' is set to 'high'.
  */
-	Configure::write('Security.level', 'medium');
+	Configure::write('Security.level', 'high');
 /**
  * A random string used in security hashing methods.
  */
