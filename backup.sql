b47c926142f8c2c08285c2a88bb7de9c45b0aa03
lboynton <notlee@gmail.com>
2008-09-02 16:18:37 +0000

    Latest database schema
    git-svn-id: svn+ssh://192.168.0.8/svn/backup/trunk@299 b4e1a2f0-0047-11dd-a6bb-41aab4003810

--- backup.sql
+++ backup.sql
@@ -13,6 +13,7 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
 --
 -- Create schema `backup`
 --
@@ -21,9 +22,25 @@
 USE `backup`;
 
 --
--- Definition of table `backups`
+-- Definition of table `backup_folders`
 --
 
+DROP TABLE IF EXISTS `backup_folders`;
+CREATE TABLE `backup_folders` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `parent_id` int(11) default NULL,
+  `lft` int(11) default NULL,
+  `rght` int(11) default NULL,
+  `name` varchar(255) NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
+
+--
+-- Definition of table `backups`
+--
 
 DROP TABLE IF EXISTS `backups`;
 CREATE TABLE `backups` (
@@ -35,8 +52,10 @@ CREATE TABLE `backups` (
   `hash` varchar(40) default NULL,
   `modified` datetime default NULL,
   `type` varchar(9) NOT NULL default 'file',
+  `path` varchar(255) NOT NULL default '',
+  `backup_folder_id` int(10) unsigned default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=102 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
 -- Definition of table `users`
@@ -54,6 +73,18 @@ CREATE TABLE `users` (
   PRIMARY KEY  (`id`),
   UNIQUE KEY `UNIQUE` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`,`username`,`password`,`email`,`created`,`real_name`,`admin`) VALUES 
+ (0,'lee','d3521f0f4841ff1a777252f1d0ed1671236ae505','lee@lboynton.com','2008-07-22 22:50:55','Lee Boynton',1),
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+
+
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
