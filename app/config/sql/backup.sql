81daeb4a4883ed004d10d33d49c12d5ea7588fba
lboynton <notlee@gmail.com>
2008-09-28 14:35:12 +0000

    Latest DB dump. Added a script to dump the db structure automatically.
    git-svn-id: svn+ssh://192.168.0.8/svn/backup/trunk@340 b4e1a2f0-0047-11dd-a6bb-41aab4003810

--- app/config/sql/backup.sql
+++ app/config/sql/backup.sql
@@ -1,30 +1,28 @@
--- MySQL Administrator dump 1.4
+-- MySQL dump 10.11
 --
+-- Host: localhost    Database: backup
 -- ------------------------------------------------------
 -- Server version	5.0.51a-3ubuntu5.3
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
---
--- Create schema `backup`
---
-
-CREATE DATABASE IF NOT EXISTS `backup`;
-USE `backup`;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Definition of table `backup`.`backups`
+-- Table structure for table `backups`
 --
-CREATE TABLE  `backup`.`backups` (
+
+DROP TABLE IF EXISTS `backups`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `backups` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `parent_id` int(11) default NULL,
   `lft` int(11) default NULL,
@@ -35,14 +33,33 @@ CREATE TABLE  `backup`.`backups` (
   `modified` datetime NOT NULL,
   `size` int(10) unsigned default NULL,
   `type` varchar(255) NOT NULL,
+  `hash` varchar(32) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
-
+) ENGINE=InnoDB AUTO_INCREMENT=964 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
--- Definition of table `backup`.`users`
+-- Table structure for table `site_parameters`
 --
-CREATE TABLE  `backup`.`users` (
+
+DROP TABLE IF EXISTS `site_parameters`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `site_parameters` (
+  `key` varchar(255) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  PRIMARY KEY  (`key`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Stores settings for the backup system';
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `username` varchar(20) NOT NULL,
   `password` varchar(40) NOT NULL,
@@ -55,14 +72,9 @@ CREATE TABLE  `backup`.`users` (
   `disabled` tinyint(1) NOT NULL default '0',
   PRIMARY KEY  (`id`),
   UNIQUE KEY `UNIQUE` (`username`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `backup`.`users`
---
-INSERT INTO `backup`.`users` (`id`,`username`,`password`,`email`,`created`,`real_name`,`admin`,`quota`,`last_login`,`disabled`) VALUES 
-  (1,'lee','d3521f0f4841ff1a777252f1d0ed1671236ae505','lee@lboynton.com','2008-09-12 17:17:27','Lee Boynton',1,10485760,'2008-09-20 18:07:17',0);
-
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -70,4 +82,8 @@ INSERT INTO `backup`.`users` (`id`,`username`,`password`,`email`,`created`,`real
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2008-09-28 14:32:27
+INSERT INTO `backup`.`users` (`id`,`username`,`password`,`email`,`created`,`real_name`,`admin`,`quota`,`last_login`,`disabled`) VALUES
+  (1,'lee','d3521f0f4841ff1a777252f1d0ed1671236ae505','lee@lboynton.com','2008-09-12 17:17:27','Lee Boynton',1,10485760,'2008-09-20 18:07:17',0);
---
+++ app/config/sql/dumpdb.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo Dumping database structure...
+mysqldump backup --user backup --password --no-data > backup.sql
+
+echo Adding user to users table...
+# add user to users table
+echo "INSERT INTO \`backup\`.\`users\` (\`id\`,\`username\`,\`password\`,\`email\`,\`created\`,\`real_name\`,\`admin\`,\`quota\`,\`last_login\`,\`disabled\`) VALUES
+  (1,'lee','d3521f0f4841ff1a777252f1d0ed1671236ae505','lee@lboynton.com','2008-09-12 17:17:27','Lee Boynton',1,10485760,'2008-09-20 18:07:17',0);" >> backup.sql
+
+echo Finished!
