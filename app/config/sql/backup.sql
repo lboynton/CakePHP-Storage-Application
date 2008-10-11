f7a8b91faac86382f916757047db35923fcc8fe8
lboynton <notlee@gmail.com>
2008-10-11 15:09:18 +0000

    Updated db dump
    git-svn-id: svn+ssh://192.168.0.8/svn/backup/trunk@355 b4e1a2f0-0047-11dd-a6bb-41aab4003810

--- app/config/sql/backup.sql
+++ app/config/sql/backup.sql
@@ -35,7 +35,7 @@ CREATE TABLE `backups` (
   `type` varchar(255) NOT NULL,
   `hash` varchar(32) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=964 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1012 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -50,6 +50,24 @@ CREATE TABLE `site_parameters` (
   `value` varchar(255) NOT NULL,
   PRIMARY KEY  (`key`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Stores settings for the backup system';
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `tickets`
+--
+
+DROP TABLE IF EXISTS `tickets`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `tickets` (
+  `id` int(11) NOT NULL auto_increment,
+  `hash` varchar(255) default NULL,
+  `data` varchar(255) default NULL,
+  `created` datetime default NULL,
+  `modified` datetime default NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `hashs` (`hash`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -71,8 +89,8 @@ CREATE TABLE `users` (
   `last_login` datetime default NULL,
   `disabled` tinyint(1) NOT NULL default '0',
   PRIMARY KEY  (`id`),
-  UNIQUE KEY `UNIQUE` (`username`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  UNIQUE KEY `UNIQUE` USING BTREE (`username`,`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -84,6 +102,6 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2008-09-28 14:32:27
+-- Dump completed on 2008-10-11 15:08:24
 INSERT INTO `backup`.`users` (`id`,`username`,`password`,`email`,`created`,`real_name`,`admin`,`quota`,`last_login`,`disabled`) VALUES
   (1,'lee','d3521f0f4841ff1a777252f1d0ed1671236ae505','lee@lboynton.com','2008-09-12 17:17:27','Lee Boynton',1,10485760,'2008-09-20 18:07:17',0);
